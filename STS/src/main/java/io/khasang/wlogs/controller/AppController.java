55ed7410375a758099ca88540a217f67a7c767bc
DmitriyAl <dvalbot@mail.ru>
2016-02-11 14:54:12 +0300

    Initial commit with project structure.

---
+++ STS/src/main/java/io/khasang/wlogs/controller/AppController.java
@@ -0,0 +1,27 @@
+package io.khasang.wlogs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Welcome to our best wLogs!");
+        model.addAttribute("tagline", "The one and only amazing logs system!");
+        return "welcome";
+    }
+
+//    @RequestMapping("/backup")
+//    public String backup(Model model) {
+//        model.addAttribute("backup", "Success");
+//        return "backup";
+//    }
+//
+//    @RequestMapping("/admin")
+//    public String admin(Model model) {
+//        model.addAttribute("admin", "You are number 1!");
+//        return "admin";
+//    }
+}

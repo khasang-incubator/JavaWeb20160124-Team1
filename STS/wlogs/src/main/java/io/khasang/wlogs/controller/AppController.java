a7aeccb942c9dfc7c08f937e6f974ca068bb9bf3
Guba Andrei <xg.andrew@gmail.com>
2016-02-13 22:56:49 +1000

    injected LogModel and LogRepository

--- STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
@@ -1,12 +1,31 @@
 package io.khasang.wlogs.controller;
 
+import io.khasang.wlogs.model.LogManager;
+import io.khasang.wlogs.model.LogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AppController {
+    @Autowired
+    private LogManager logManager;
+    @Autowired
+    private LogRepository logRepository;
+
+    public void setLogManager(LogManager logManager) {
+        this.logManager = logManager;
+    }
+
+    public void setLogRepository(LogRepository logRepository) {
+        this.logRepository = logRepository;
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
         return "index";
@@ -18,9 +37,15 @@ public class AppController {
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String deleteAction(Model model) {
-        model.addAttribute("process", "Processing of log deleting...");
-        return "delete";
+    public String deleteAction(HttpServletRequest request, RedirectAttributes redirectAttributes, Model model) {
+        try {
+            Integer periodCriteriaId = Integer.valueOf(request.getParameter("period_criteria_id"));
+            Integer affectedRows = logManager.delete(periodCriteriaId);
+            redirectAttributes.addFlashAttribute("success", "Success deleted " + affectedRows);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:delete";
     }
 
     @RequestMapping("/import")

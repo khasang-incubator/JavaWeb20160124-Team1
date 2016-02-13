f0d69b276675f73844d6c6bc8f7f9a8e7eb9429c
Guba Andrei <xg.andrew@gmail.com>
2016-02-13 23:50:53 +1000

    implemented findAll method

--- STS/wlogs/src/main/java/io/khasang/wlogs/model/LogRepository.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/LogRepository.java
@@ -1,7 +1,12 @@
 package io.khasang.wlogs.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class LogRepository {
     private JdbcTemplate jdbcTemplate;
@@ -13,5 +18,20 @@ public class LogRepository {
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public ArrayList<LogModel> findAll(Integer limit, Integer offset) {
+        String sql = "SELECT * FROM :tableName ORDER BY occurred_at DESC LIMIT :limit OFFSET :offset"
+                .replace(":tableName", tableName).replace(":limit", limit.toString()).replace(":offset", offset.toString());
+        return jdbcTemplate.query(sql, new ResultSetExtractor<ArrayList<LogModel>>() {
+            @Override
+            public ArrayList<LogModel> extractData(ResultSet rs) throws SQLException, DataAccessException {
+                ArrayList<LogModel> logs = new ArrayList<>();
+                while(rs.next()) {
+                    logs.add(LogModel.createFromResultSet(rs));
+                }
+                return logs;
+            }
+        });
     }
 }

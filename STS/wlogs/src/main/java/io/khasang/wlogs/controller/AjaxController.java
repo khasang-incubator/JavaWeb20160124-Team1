74efd54bae3dc8f5d0ebdeaa6f68ba880b9208e5
Guba Andrei <xg.andrew@gmail.com>
2016-02-14 03:03:06 +1000

    implemented controller to process ajax requests

---
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/AjaxController.java
@@ -0,0 +1,35 @@
+package io.khasang.wlogs.controller;
+
+import io.khasang.wlogs.model.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RequestMapping("/ajax")
+@Controller
+public class AjaxController {
+    @Autowired
+    private LogManager logManager;
+
+    public void setLogManager(LogManager logManager) {
+        this.logManager = logManager;
+    }
+
+    @RequestMapping(value = "/import-fixtures", produces = "application/json", method = RequestMethod.GET)
+    @ResponseBody
+    public String loadFixtures(HttpServletResponse response) {
+        String responseBody = "";
+        try {
+            logManager.loadFixtures();
+            responseBody = "{\"status\":\"OK\"}";
+        } catch (Exception e) {
+            responseBody = "{\"status\":\"error\", \"error\":\"" + e.getMessage() + "\"}";
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        return responseBody;
+    }
+}

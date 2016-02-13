5f05d693fa02880df98e363c00baf5a10bd2ef5c
Guba Andrei <xg.andrew@gmail.com>
2016-02-13 23:51:51 +1000

    removed unused annotation import

--- STS/wlogs/src/main/java/io/khasang/wlogs/model/LogManager.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/LogManager.java
@@ -1,6 +1,5 @@
 package io.khasang.wlogs.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.*;
 

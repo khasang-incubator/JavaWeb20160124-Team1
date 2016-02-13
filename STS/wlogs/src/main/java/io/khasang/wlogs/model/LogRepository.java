76b3ce0bb2be083fa78e7b3e7ef050f500cfa888
Guba Andrei <xg.andrew@gmail.com>
2016-02-13 22:55:02 +1000

    added LogRepository, while empty, class will do search by different criteria.

---
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/LogRepository.java
@@ -0,0 +1,12 @@
+package io.khasang.wlogs.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class LogRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}

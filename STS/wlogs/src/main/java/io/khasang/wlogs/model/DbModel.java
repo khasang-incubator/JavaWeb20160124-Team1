58e47049d820448c3cdec1ce79550a56fb8d227a
VSB <330034@gmail.com>
2016-02-13 20:35:43 +0600

    db and table model

--- STS/wlogs/src/main/java/io/khasang/wlogs/model/DbModel.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/DbModel.java
@@ -1,7 +1,60 @@
 package io.khasang.wlogs.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by VSB on 13.02.2016.
  */
 public class DbModel {
+    private Connection connection;
+    private String host = "localhost";
+    private String portNumber = "3306";
+    private String databaseName = "dbname";
+    private String url = "jdbc:mysql://" + host + ":" + portNumber + "/" + databaseName;
+    private String userName = "dbuser";
+    private String password = "dbuserpassword";
+    private String error;
+    private Statement statement;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Connection getConnection() {
+        connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return null;
+        } catch (InstantiationException e) {
+            //e.printStackTrace();
+            return null;
+        } catch (IllegalAccessException e) {
+            //e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+            return null;
+        }
+        return connection;
+    }
+
+    public Statement getStatement() {
+        try {
+            Connection connection = getConnection();
+            if (connection != null)
+                return connection.createStatement();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
 }
--- STS/wlogs/src/main/java/io/khasang/wlogs/model/TestTableModel.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/TestTableModel.java
@@ -1,7 +1,42 @@
 package io.khasang.wlogs.model;
 
-/**
- * Created by VSB on 13.02.2016.
- */
 public class TestTableModel {
+    public enum tableFiled {server, date, issue, comment}
+
+    private String server;
+    private String date;
+    private String issue;
+    private String comment;
+
+    public String getServer() {
+        return server;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }

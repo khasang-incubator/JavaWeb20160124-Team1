e5075c2ee2754ee80463949359f586c42f48d1fc
Guba Andrei <xg.andrew@gmail.com>
2016-02-17 20:43:16 +1000

    added js script to fill select form element

--- STS/wlogs/web/WEB-INF/views/js/app.js
+++ STS/wlogs/web/WEB-INF/views/js/app.js
@@ -1,5 +1,37 @@
 /* Import page, AJAX loading of fixtures */
 $(document).ready(function() {
+    var dateIntervalSizeSelect = $('#date_interval_size');
+    var dateIntervalTypeSelect = $('#date_interval_id');
+    if (dateIntervalSizeSelect.length && dateIntervalTypeSelect.length) {
+        fillDateIntervalSize(7);
+        dateIntervalTypeSelect.change(function() {
+            var type = parseInt(dateIntervalTypeSelect.val());
+            switch (type) {
+                case 0:
+                    fillDateIntervalSize(7); //Days
+                    break;
+                case 1:
+                    fillDateIntervalSize(4); //Weeks
+                    break;
+                case 2:
+                    fillDateIntervalSize(12); //Month
+                    break;
+                case 3:
+                    fillDateIntervalSize(10); //Year
+                    break;
+            }
+        });
+
+        function fillDateIntervalSize(maxSize) {
+            if (dateIntervalSizeSelect.length) {
+                $(dateIntervalSizeSelect).html('');
+                for (var i = 1; i <= maxSize; i++) {
+                    $('<option/>', {value: i, html: i}).appendTo(dateIntervalSizeSelect);
+                }
+            }
+        }
+    }
+
     var createTableBtn = $('#createTableBtn');
     createTableBtn.children('img').hide();
     createTableBtn.click(function() {

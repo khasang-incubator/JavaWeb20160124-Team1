b7c2b9b32d85f8a7c631278626c771a374e07e81
DmitriyAl <dvalbot@mail.ru>
2016-02-18 18:58:22 +0300

    Login site creation. Table "users" was added.

--- STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
@@ -1,9 +1,6 @@
 package io.khasang.wlogs.controller;
 
-import io.khasang.wlogs.model.InsertDataTable;
-import io.khasang.wlogs.model.LogManager;
-import io.khasang.wlogs.model.LogRepository;
-import io.khasang.wlogs.model.ViewDataTable;
+import io.khasang.wlogs.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -143,6 +140,8 @@ public class AppController {
 
     @RequestMapping("login") //todo dalbot
     public String login(Model model) {
+        JdbcInterface login = new Login();
+        login.createTable();
         model.addAttribute("login", "Login Users"); //return user list from current logon name, db with id, username, role, description
         return "login";
     }
--- STS/wlogs/src/main/java/io/khasang/wlogs/model/Login.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/Login.java
@@ -1,6 +1,36 @@
 package io.khasang.wlogs.model;
 
 
-public class Login {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
+import java.util.LinkedList;
+import java.util.List;
+
+public class Login implements JdbcInterface {
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+
+    }
+
+    @Override
+    public void createTable() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUsername("root");
+        dataSource.setUrl("jdbc:mysql://localhost/wlogs");
+        dataSource.setPassword("root");
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("DROP TABLE IF EXISTS users");
+        jdbcTemplate.execute("create table users(ID INT, login VARCHAR(40), password VARCHAR(40), description VARCHAR (100));");
+        jdbcTemplate.update("INSERT INTO users (login, password) VALUES ('user1', 'pas1');");
+        jdbcTemplate.update("INSERT INTO users (login, password) VALUES ('user2', 'pas2');");
+    }
+
+//    public List<User> takeUsers() {
+//        List<User> users = new LinkedList<User>();
+//        jdbcTemplate.
+//    }
 }

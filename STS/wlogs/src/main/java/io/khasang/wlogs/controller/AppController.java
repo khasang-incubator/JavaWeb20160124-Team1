1c3060909e3be37b4ddefe9c6202f2a19854075a
Kuznetsov Sergey <azon.sk@gmail.com>
2016-02-22 20:12:11 +0300

    mapping table to refactor

--- STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 
 @Controller
 public class AppController {
@@ -193,7 +192,7 @@ public class AppController {
         return "admin";
     }
 
-    @RequestMapping("/table")
+    /*@RequestMapping("/table")
     public String table(Model model) {
         model.addAttribute("table", "You have one table!");
         DbModel db = new DbModel();
@@ -206,7 +205,7 @@ public class AppController {
             db.setError(null);
         }
         return "table";
-    }
+    }*/
 
 
     /*sorlov work*/

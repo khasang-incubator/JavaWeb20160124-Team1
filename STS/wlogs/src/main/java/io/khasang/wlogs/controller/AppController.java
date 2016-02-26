873dbe817dc41394e2a7e3d7407cc3580a962282
Guba Andrei <xg.andrew@gmail.com>
2016-02-27 04:39:27 +1000

    added profile mapping

--- STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/AppController.java
@@ -4,6 +4,7 @@ import io.khasang.wlogs.form.DeleteDataForm;
 import io.khasang.wlogs.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class AppController {
@@ -221,5 +223,11 @@ public class AppController {
     public String selectData(Model model) {
         model.addAttribute("items", viewDataFromTable.selectWholeTable(tableObjectInterface));
         return "select";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(Model model, UsernamePasswordAuthenticationToken principal) {
+        model.addAttribute("principal", principal.getPrincipal());
+        return "profile";
     }
 }

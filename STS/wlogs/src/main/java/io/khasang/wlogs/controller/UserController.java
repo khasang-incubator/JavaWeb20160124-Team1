957fce362468ba77cbcb00ec46cd67fb7216ae5e
Guba Andrei <xg.andrew@gmail.com>
2016-02-28 04:09:27 +1000

    catch errors on user creation

--- STS/wlogs/src/main/java/io/khasang/wlogs/controller/UserController.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,7 +32,8 @@ public class UserController {
         try {
             this.userManager.create(userRegistrationForm);
         } catch (Exception e) {
-
+            bindingResult.addError(new ObjectError("userRegistrationForm", e.getMessage()));
+            return "wlogs_user_registration";
         }
         return "redirect:auth/login";
     }

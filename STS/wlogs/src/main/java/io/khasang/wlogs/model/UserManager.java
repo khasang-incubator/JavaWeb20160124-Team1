06d8ec5d62052344e8b4f358c09fd3ea091ea4e2
Guba Andrei <xg.andrew@gmail.com>
2016-02-28 05:19:52 +1000

    encode password before create user

--- STS/wlogs/src/main/java/io/khasang/wlogs/model/UserManager.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/model/UserManager.java
@@ -2,8 +2,10 @@ package io.khasang.wlogs.model;
 
 import io.khasang.wlogs.form.UserRegistrationForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.sql.PreparedStatement;
@@ -16,13 +18,16 @@ public class UserManager {
     private UserRepository userRepository;
     @Autowired
     private InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    @Autowired
+    @Qualifier("bcryptPasswordEncoder")
+    private PasswordEncoder passwordEncoder;
 
     public void create(UserRegistrationForm userRegistrationForm) throws Exception {
         if (this.inMemoryUserDetailsManager.userExists(userRegistrationForm.getUsername()) ||
             this.userRepository.userExists(userRegistrationForm.getUsername())) {
             throw new Exception("User with such name already exists. Try another one.");
         }
-        this.createUser(userRegistrationForm.getUsername(), userRegistrationForm.getPassword());
+        this.createUser(userRegistrationForm.getUsername(), passwordEncoder.encode(userRegistrationForm.getPassword()));
     }
 
     private void createUser(String username, String password) {

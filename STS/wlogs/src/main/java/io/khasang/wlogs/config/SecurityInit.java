1777d1ed28628b49b4f77e8a31c77f7b9d3bf339
Guba Andrei <xg.andrew@gmail.com>
2016-03-01 05:02:45 +1000

    added security initializer class

---
+++ STS/wlogs/src/main/java/io/khasang/wlogs/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package io.khasang.wlogs.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}

4afa3fa1282257de7162f22e41d7b3dae7650a32
Guba Andrei <xg.andrew@gmail.com>
2016-03-01 05:52:47 +1000

    configured ignored paths for security component, configured http zone

--- STS/wlogs/src/main/java/io/khasang/wlogs/config/SecurityConfig.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/config/SecurityConfig.java
@@ -3,33 +3,48 @@ package io.khasang.wlogs.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.access.vote.AuthenticatedVoter;
 import org.springframework.security.access.vote.RoleHierarchyVoter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@Import(AppConfig.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     protected DriverManagerDataSource dataSource;
     @Autowired
     protected BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
-    protected DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler;
+    protected SecurityExpressionHandler<FilterInvocation> defaultWebSecurityExpressionHandler;
+    @Autowired
+    protected AffirmativeBased accessDesignManager;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                        .antMatchers("/css/**", "/js/**", "/images/**");
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -50,26 +65,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                        .antMatchers("/auth/login").anonymous()
-                        .antMatchers(HttpMethod.POST, "/users").anonymous()
-                        .antMatchers(HttpMethod.GET, "/users").anonymous()
-                        .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                        .antMatchers("/**").fullyAuthenticated()
+                        .accessDecisionManager(this.accessDesignManager)
                         .expressionHandler(this.defaultWebSecurityExpressionHandler)
+                        .antMatchers("/auth/login").anonymous()
+                        .antMatchers("/users").anonymous()
+                        .anyRequest().fullyAuthenticated()
                         .and()
                 .formLogin()
                         .loginPage("/auth/login")
                         .defaultSuccessUrl("/")
                         .and()
-                .logout()
-                        .and()
-                ;
+                .logout();
     }
 
     @Bean
     public RoleHierarchyImpl roleHierarchy() {
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("SUPER_ADMIN > ADMIN > USER");
+        roleHierarchy.setHierarchy("ROLE_SUPER_ADMIN > ROLE_ADMIN > ROLE_USER");
         return roleHierarchy;
     }
 
@@ -79,14 +91,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler(RoleHierarchyImpl roleHierarchy) {
+    public SecurityExpressionHandler<FilterInvocation> defaultWebSecurityExpressionHandler(RoleHierarchyImpl roleHierarchy) {
         DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler = new DefaultWebSecurityExpressionHandler();
         defaultWebSecurityExpressionHandler.setRoleHierarchy(roleHierarchy);
         return defaultWebSecurityExpressionHandler;
     }
 
     @Bean
-    public AffirmativeBased accessDesignManager(RoleHierarchyVoter roleVoter, DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler) {
+    public AffirmativeBased accessDesignManager(RoleHierarchyVoter roleVoter, SecurityExpressionHandler<FilterInvocation> defaultWebSecurityExpressionHandler) {
         List<AccessDecisionVoter<? extends Object>> args = new ArrayList<>();
         WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
         webExpressionVoter.setExpressionHandler(defaultWebSecurityExpressionHandler);

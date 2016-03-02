dc338caddd84dfce020c2845c9d1dff945f5ce17
Guba Andrei <xg.andrew@gmail.com>
2016-03-02 15:01:54 +1000

    override dispatcher registration to init throwExceptionIfNoHandlerFound param

--- STS/wlogs/src/main/java/io/khasang/wlogs/app/initializer/SpringInitializer.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/app/initializer/SpringInitializer.java
@@ -3,6 +3,8 @@ package io.khasang.wlogs.app.initializer;
 import io.khasang.wlogs.app.config.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletRegistration;
+
 public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -17,5 +19,10 @@ public class SpringInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
     }
 }

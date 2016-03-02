fb0cfee3642102ab14627e435c87403dd4db54a6
Guba Andrei <xg.andrew@gmail.com>
2016-03-02 14:59:34 +1000

    removed default dispatcher configurer

--- STS/wlogs/src/main/java/io/khasang/wlogs/app/config/WebConfig.java
+++ STS/wlogs/src/main/java/io/khasang/wlogs/app/config/WebConfig.java
@@ -1,14 +1,13 @@
 package io.khasang.wlogs.app.config;
 
 import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.util.List;
 import java.util.Properties;
 
 @EnableWebMvc
@@ -17,11 +16,6 @@ import java.util.Properties;
 public class WebConfig extends WebMvcConfigurerAdapter {
     private static final String VIEW_RESOLVER_PREFIX = "/WEB-INF/views/";
     private static final String VIEW_RESOLVER_SUFFIX = ".jsp";
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
 
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
@@ -39,14 +33,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
         registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/views/images/");
         registry.addResourceHandler("/favicon.ico").addResourceLocations("/WEB-INF/views/images/");
-    }
-
-    @Bean
-    public SimpleMappingExceptionResolver exceptionResolver() {
-        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
-        Properties statusCodes = new Properties();
-        statusCodes.put("/404", "404");
-        exceptionResolver.setStatusCodes(statusCodes);
-        return exceptionResolver;
     }
 }
